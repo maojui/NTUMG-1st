e1541404d102f47b3b12469113678ecd98aeeabd
Neptune <bearbear9117@gmail.com>
2018-05-21 01:35:51 +0800

    FIXED : Cannot execute my server QwQ

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 app_run.py
+web: gunicorn firstpack.app_run
---
+++ firstpack/app_run.py
@@ -0,0 +1,13 @@
+from japronto import Application
+from jinja2 import Template
+
+
+template = Template('<h1>Hello {{ name }}!</h1>')
+
+def hello(request):
+    return request.Response(text=template.render(name='World'), 
+                            mime_type='text/html')
+
+app = Application()
+app.router.add_route('/', hello)
+app.run(port=8080,debug=False)
