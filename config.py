f15383600eceb73e08e77aefce274d4b83822220
Neptune <bearbear9117@gmail.com>
2018-05-21 23:26:26 +0800

    ADD : Fixed ucomfortable

--- config.py
+++ config.py
@@ -2,114 +2,141 @@
 picturelist = [
     {
         "file":"1-1.jpg",
-        "name":"AMAZING MIGHTYYYY!!!!/WAiKURO",
+        "name":"AMAZING MIGHTYYYY!!!!",
+        "author":"WAiKURO",
         "level":"18",
         "judge":"強力",
         "effector":"RiS.DVVVV!!!!",
-        "comment":"驚いたか、その強く、美しい力！",
+        "comment":"「驚いたか、その強く、美しい力！」",
         "category":"faith",
     },
     {
         "file":"1-2.jpg",
-        "name":"Bloody Purity/eyemedia",
+        "name":"Bloody Purity",
+        "author":"eyemedia",
         "level":"17","judge":"純正",
-        "effector":"止水@purified","comment":"「真っ白と真紅、どっちが好き？」",
+        "effector":"止水@purified",
+        "comment":"「真っ白と真紅、どっちが好き？」",
         "category":"faith"
     },
     {
         "file":"1-3.jpg",
-        "name":"GOODRAGE/EBIMAYO",
+        "name":"GOODRAGE",
+		"author":"EBIMAYO",
         "level":"18","judge":"新生",
-        "effector":"NIA1V","comment":"「今の君は、昨日の自分の望む姿か…」",
+        "effector":"NIA1V",
+        "comment":"「今の君は、昨日の自分の望む姿か…」",
         "category":"hope"
     },
     {
         "file":"1-4.jpg",
-        "name":"Lonely Departure/Chroma",
+        "name":"Lonely Departure",
+		"author":"Chroma",
         "level":"18","judge":"邁進",
-        "effector":"NEO.LPO [出発]","comment":"「もう......前に進むしか......ない。」",
+        "effector":"NEO.LPO [出発]",
+        "comment":"「もう......前に進むしか......ない。」",
         "category":"hope"
     },
     {
         "file":"1-5.jpg",
-        "name":"魔法少女幸福論/トーマ",
+        "name":"魔法少女幸福論",
+		"author":"トーマ",
         "level":"18","judge":"奇跡",
-        "effector":"魔法少女YOSORO幸福戦","comment":"「自分を失くした、愛の侵略者よ！」",
+        "effector":"魔法少女YOSORO幸福戦",
+        "comment":"「自分を失くした、愛の侵略者よ！」",
         "category":"faith"
     },
     {
         "file":"1-6.jpg",
-        "name":"運命論/ルゼ ☆ えみゅう。 ☆ 石王マサト",
+        "name":"運命論",
+		"author":"ルゼ ☆ えみゅう。☆ 石王マサト",
         "level":"??","judge":"宿命",
-        "effector":"孤高神TurTleT","comment":"「せいぜい生きろ、その宿命の果てに。」",
+        "effector":"孤高神TurTleT",
+        "comment":"「せいぜい生きろ、その宿命の果てに。」",
         "category":"faith"
     },
     {
         "file":"1-7.jpg",
-        "name":"Angel Wing/uma",
+        "name":"Angel Wing",
+		"author":"uma",
         "level":"19","judge":"展翅",
-        "effector":"天使M99","comment":"「無限の空　羽が広げ」",
+        "effector":"天使M99",
+        "comment":"「無限の空　羽が広げ」",
         "category":"hope"
     },
     {
         "file":"1-8.jpg",
-        "name":"☆LuminouS StellA☆/sak×にっすぃー ",
+        "name":"☆LuminouS StellA☆",
+		"author":"sak×にっすぃー ",
         "level":"17","judge":"閃耀",
-        "effector":"Overjoyを目指M99","comment":"「キラキラかわいい」",
+        "effector":"Overjoyを目指M99",
+        "comment":"「キラキラかわいい」",
         "category":"hope"
     },
     {
         "file":"1-9.jpg",
-        "name":"ダンスアリスダンス/れいめん ",
+        "name":"ダンスアリスダンス",
+		"author":"れいめん ",
         "level":"17","judge":"幼女",
-        "effector":"ニゴウ","comment":"「アリスはJKじゃい！」",
+        "effector":"ニゴウ","comment":
+        "「アリスはJKじゃい！」",
         "category":"heaven"
     },
     {
         "file":"1-10.jpg",
-        "name":"Anti the EuphoriaHOLiC/cosMo＠暴走P ",
+        "name":"Anti the EuphoriaHOLiC",
+		"author":"cosMo＠暴走P ",
         "level":"19","judge":"楽園",
-        "effector":"Dr. ΖΑΩ","comment":"「でも、それはほんとうに'しあわせ'なのでしょうか？」",
+        "effector":"Dr. ΖΑΩ",
+        "comment":"「でも、それはほんとうに'しあわせ'なのでしょうか？」",
         "category":"heaven"
     },
     {
         "file":"1-11.jpg",
-        "name":"SadisticMusic∞Factory/cosMo＠暴走P",
+        "name":"SadisticMusic∞Factory",
+		"author":"cosMo＠暴走P",
         "level":"19","judge":"絕望",
         "effector":"からかい上手のモニカちゃん","comment":"「......絶対逃ガサナイカラ。」",
         "category":"final"
     },
     {
         "file":"1-12.jpg",
-        "name":"妖艶魔女 ~trappola bewitching~/gmtn",
+        "name":"妖艶魔女 ~trappola bewitching~",
+		"author":"gmtn",
         "level":"18","judge":"混乱",
-        "effector":"SEXY FUMIYA in Abyss","comment":"「❤❤❤悪意に満ちている世界をうけとれ❤❤❤」",
+        "effector":"SEXY FUMIYA in Abyss",
+        "comment":"「❤❤❤悪意に満ちている世界をうけとれ❤❤❤」",
         "category":"final"
     },
     {
         "file":"1-13.jpg",
-        "name":"Berry Go!!/Freezer feat. 妃苺 ",
+        "name":"Berry Go!!",
+		"author":"Freezer feat. 妃苺 ",
         "level":"17","judge":"甘味",
         "effector":"PERCIA","comment":"「それを食べると、甘すっぱく感じする苺ですよ。」",
         "category":"heaven"
     },
     {
         "file":"1-14.jpg",
-        "name":"花雪/smileY inc.",
+        "name":"花雪",
+		"author":"smileY inc.",
         "level":"16","judge":"花舞",
         "effector":"関谷なる","comment":"「わたしと一緒に踊ってくれますか？」",
         "category":"heaven"
     },
     {
         "file":"1-15.jpg",
-        "name":"Devil's Gear/DJ Myosuke",
+        "name":"Devil's Gear",
+		"author":"DJ Myosuke",
         "level":"18","judge":"狂喜",
-        "effector":"悪魔女王XxdbxX","comment":"「ウフフ...己の結末に、向かう準備はもうできたかしら～？」",
+        "effector":"悪魔女王XxdbxX",
+        "comment":"「ウフフ...己の結末に、向かう準備はもうできたかしら~?」",
         "category":"final"
     },
     {
         "file":"1-16.jpg",
-        "name":"アズールレーン「VS夕立」/Jimmy'z",
+        "name":"アズールレーン「VS夕立」",
+		"author":"Jimmy'z",
         "level":"18","judge":"壞滅",
         "effector":"ユニコーンのお兄ちゃん2号♪","comment":"「ソロモンの悪夢、見せてやるぜ。」",
         "category":"final"
